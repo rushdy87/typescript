94ada9e9715a7c2cde7f89c5d06e7aa0d0d9994c
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-08-06 13:14:16 +0300

    Build a Web Framework: Build class User as a 'mega' class with tons of methods

--- web/dist/src.f10117fe.js
+++ web/dist/src.f10117fe.js
@@ -118,7 +118,11 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/index.ts":[function(require,module,exports) {
-console.log('Hi there');
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 },{}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51311" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62472" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
